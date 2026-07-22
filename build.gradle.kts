f7d26e493dd399ad03c5b0be902335ca70aaf100
drawned <drawnwylde@gmail.com>
2026-07-22 20:19:03 -0300

    fix calil

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("java-library")
-    id("xyz.jpenilla.run-paper") version "3.0.2"
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(26)
 }
 
 repositories {
@@ -28,22 +31,9 @@ dependencies {
         exclude(group = "org.spigotmc")
         exclude(group = "io.papermc.paper")
     }
-
-}
-
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(25)
 }
 
 tasks {
-    runServer {
-        // Configure the Minecraft version for our task.
-        // This is the only required configuration besides applying the plugin.
-        // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("26.2")
-        jvmArgs("-Xms2G", "-Xmx2G")
-    }
-
     processResources {
         val props = mapOf("version" to version)
         filesMatching("plugin.yml") {
